a4fed9cc9d1ecf5e76aabe7ef7100b468ac787e6
Vivien Nicolas <21@vingtetun.org>
2011-11-04 11:50:01 +0100

    Remove deb packaging

--- b2g/build.mk
+++ b2g/build.mk
@@ -74,9 +74,6 @@ install::
 	@echo "B2G can't be installed directly."
 	@exit 1
 
-deb: package
-	@$(MAKE) -C b2g/installer deb
-
 upload::
 	@$(MAKE) -C b2g/installer upload
 
@@ -92,6 +89,3 @@ mochitest:: mochitest-browser-chrome
 .PHONY: mochitest-browser-chrome
 endif
 
-ifeq ($(OS_TARGET),Linux)
-deb: installer
-endif
