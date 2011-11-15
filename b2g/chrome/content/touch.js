7cd637fa6d43fb32b4d9a05d54beb0d7ecacf4b6
Vivien Nicolas <21@vingtetun.org>
2011-11-15 15:59:42 +0100

    Fire the contextmenu event after a delay of 2 seconds

--- b2g/chrome/content/touch.js
+++ b2g/chrome/content/touch.js
@@ -1,7 +1,10 @@
 
 (function touchEventHandler() {
-  var TouchEventHandler = {
-    events: ['mousedown', 'mousemove', 'mouseup', 'mouseout', 'unload'],
+  let contextMenuTimeout = 0;
+
+  let TouchEventHandler = {
+    events: ['mousedown', 'mousemove', 'mouseup', 'mouseout',
+             'unload', 'contextmenu'],
     start: function teh_start() {
       this.events.forEach((function(evt) {
         shell.home.addEventListener(evt, this, true);
@@ -13,55 +16,74 @@
       }).bind(this));
     },
     handleEvent: function teh_handleEvent(evt) {
-      var eventTarget = this.target;
+      let eventTarget = this.target;
 
-      var type = '';
+      let type = '';
       switch (evt.type) {
         case 'mousedown':
           this.target = evt.target;
           this.timestamp = evt.timeStamp;
+          evt.target.setCapture(false);
           type = 'touchstart';
           break;
         case 'mousemove':
           // On device a mousemove event if fired right after the mousedown
           // because of the size of the finger, so let's ignore what happens
           // below 5ms
-          if (evt.timeStamp - this.timestamp < 5)
+          if (evt.timeStamp - this.timestamp < 5) {
+            let x = evt.pageX;
+            let y = evt.pageY;
+            let event = evt.target.ownerDocument.createEvent("MouseEvent");
+            event.initMouseEvent("contextmenu", true, true, content,
+                                 0, x, y, x, y, false, false, false, false,
+                                 0, null);
+            event.x = x;
+            event.y = y;
+            contextMenuTimeout = window.setTimeout(function contextMenu() {
+              evt.target.dispatchEvent(event);
+            }, 2000);
             return;
+          }
+          window.clearTimeout(contextMenuTimeout);
           type = 'touchmove';
           break;
         case 'mouseup':
           this.target = null;
+          document.releaseCapture();
           type = 'touchend';
           break;
         case 'mouseout':
           if (evt.relatedTarget)
             return;
           this.target = null;
+          document.releaseCapture();
           type = 'touchcancel';
           break;
         case 'unload':
           TouchEventHandler.stop();
           return;
+        case 'contextmenu':
+          this.target = null;
+          document.releaseCapture();
+          return;
       }
 
-      var target = eventTarget || this.target;
+      let target = eventTarget || this.target;
       if (target) {
         this.sendTouchEvent(evt, target, type);
-        evt.preventDefault();
         evt.stopPropagation();
       }
     },
     sendTouchEvent: function teh_sendTouchEvent(evt, target, name) {
-      var touchEvent = document.createEvent("touchevent");
-      var point = document.createTouch(window, target, 0,
+      let touchEvent = document.createEvent("touchevent");
+      let point = document.createTouch(window, target, 0,
                                      evt.pageX, evt.pageY,
                                      evt.screenX, evt.screenY,
                                      evt.clientX, evt.clientY,
                                      1, 1, 0, 0);
-      var touches = document.createTouchList(point);
-      var targetTouches = touches;
-      var changedTouches = touches;
+      let touches = document.createTouchList(point);
+      let targetTouches = touches;
+      let changedTouches = touches;
       touchEvent.initTouchEvent(name, true, true, window, 0,
                                 false, false, false, false,
                                 touches, targetTouches, changedTouches);
